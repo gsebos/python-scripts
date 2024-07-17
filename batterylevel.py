ba7ede961415ce6a6d42e5f6d1c89e6bfd07ca62
gsebos <sgsebastiengeorges@gmail.com>
2024-07-17 21:47:03 +0100

    scripts to output battery levels

---
+++ batterylevel.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+
+green="^c#409960^"
+amber="^c#998240^"
+red="^c#ba1818^"
+
+icons = ["󰁺","󰁻","󰁼","󰁽","󰁾","󰁿","󰂀","󰂁","󰂂","󱟢"]
+icons_colors = [red+"󰁺",red+"󰁻",red+"󰁼",amber+"󰁽",amber+"󰁾",amber+"󰁿",green+"󰂀",green+"󰂁",green+"󰂂",green+"󱟢"]
+
+def cleanup_output(raw_output):
+    clean_output = raw_output.replace("b","").replace("\\n","").replace("'","")
+    return clean_output
+
+batteries = []
+for file in os.listdir('/sys/class/hwmon/'):
+    dev_name = cleanup_output(str(subprocess.check_output(f"cat /sys/class/hwmon/{file}/name", shell=True)))
+    if "BAT" in dev_name:
+        bat = {}
+        bat["name"]= dev_name
+        bat["status"]= cleanup_output(str(subprocess.check_output(f"cat /sys/class/hwmon/{file}/device/status", shell=True)))
+        bat["full"]=int(subprocess.check_output(f"cat /sys/class/hwmon/{file}/device/energy_full", shell=True))
+        bat["now"]=int(subprocess.check_output(f"cat /sys/class/hwmon/{file}/device/energy_now", shell=True))
+        bat["perc"] = round((bat["now"] / bat["full"])*100)
+        batteries.append(bat)
+batinfo=[]
+for bat in batteries:
+    if bat["status"] == "Discharging" or bat["status"] == "Not charging":
+        icon = icons_colors[int(bat['perc']/10)]
+    else:
+        icon = "^c#f2faf4^󰂄"
+    batinfo.append(f"{icon} {bat['name']}:{bat['perc']}%")
+
+print(" ".join(batinfo))
+print(batteries[0]["status"])

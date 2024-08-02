1447cb3102ccfb351222a0ae52302227aa7ab8ac
gsebos <sgsebastiengeorges@gmail.com>
2024-08-02 16:06:05 +0100

    adds no-colors and no-icons options

--- batterylevel.py
+++ batterylevel.py
@@ -6,16 +6,43 @@ parser = argparse.ArgumentParser(
     prog="Battery levels simple script",
     description="reads the hwmon sys files to detect batteries input and outputs battery levels"
 )
-parser.add_argument("--icons",type=bool,default=True)
-parser.add_argument("--colors",type=bool,default=False)
 
-# status2d colors
-green="^c#409960^"
-amber="^c#998240^"
-red="^c#ba1818^"
+parser.add_argument("--icons",action='store_true')
+parser.add_argument("--no-icons",dest='icons',action='store_false')
+parser.add_argument("--colors",action='store_true')
+parser.add_argument("--no-colors",dest='colors',action='store_false')
+parser.set_defaults(icons=True,colors=True)
+args = parser.parse_args()
 
-icons = ["󰁺","󰁻","󰁼","󰁽","󰁾","󰁿","󰂀","󰂁","󰂂","󱟢"]
-icons_colors = [red+"󰁺",red+"󰁻",red+"󰁼",amber+"󰁽",amber+"󰁾",amber+"󰁿",green+"󰂀",green+"󰂁",green+"󰂂",green+"󱟢"]
+
+if args.colors:
+    green="^c#409960^"
+    amber="^c#998240^"
+    red="^c#ba1818^"
+    white="^c#f2faf4^"
+else:
+    green=""
+    amber=""
+    red=""
+    white=""
+
+if args.icons:
+    icons = ["󰁺","󰁻","󰁼","󰁽","󰁾","󰁿","󰂀","󰂁","󰂂","󱟢"]
+else:
+    icons = ["","","","","","","","","",""]
+
+icons_colors = [
+    red+icons[0],
+    red+icons[1],
+    red+icons[2],
+    amber+icons[3],
+    amber+icons[4],
+    amber+icons[5],
+    green+icons[6],
+    green+icons[7],
+    green+icons[8],
+    green+icons[9]
+    ]
 
 def cleanup_output(raw_output):
     clean_output = raw_output.replace("b","").replace("\\n","").replace("'","")
@@ -34,11 +61,11 @@ for file in os.listdir('/sys/class/hwmon/'):
         batteries.append(bat)
 batinfo=[]
 for bat in batteries:
-    if bat["status"] == "Discharging" or bat["status"] == "Not charging":
-        icon = icons_colors[int(bat['perc']/10)]
-    else:
-        icon = "^c#f2faf4^󰂄"
-    batinfo.append(f"{icon} {bat['name']}:{bat['perc']}%")
+        if bat["status"] == "Discharging" or bat["status"] == "Not charging":
+            icon = icons_colors[int(bat['perc']/10)]
+        else:
+            icon = f"{white}󰂄"
+        batinfo.append(f"{icon} {bat['name']}:{bat['perc']}%")
 
 print(" ".join(batinfo))
-print(batteries[0]["status"])
+# print(batteries[0]["status"])

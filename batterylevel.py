a3090b45f700d86dc884023e311ff34c6b1cacc2
gsebos <sgsebastiengeorges@gmail.com>
2024-08-05 10:49:43 +0100

    moves code to main function

--- batterylevel.py
+++ batterylevel.py
@@ -2,50 +2,6 @@ import subprocess
 import os
 import argparse
 
-parser = argparse.ArgumentParser(
-    prog="Battery levels simple script",
-    description="reads the hwmon sys files to detect batteries input and outputs battery levels"
-)
-
-parser.add_argument("--icons",action='store_true')
-parser.add_argument("--no-icons",dest='icons',action='store_false')
-parser.add_argument("--colors",action='store_true')
-parser.add_argument("--no-colors",dest='colors',action='store_false')
-parser.set_defaults(icons=True,colors=True)
-args = parser.parse_args()
-
-
-if args.colors:
-    green="^c#409960^"
-    amber="^c#998240^"
-    red="^c#ba1818^"
-    white="^c#f2faf4^"
-else:
-    green=""
-    amber=""
-    red=""
-    white=""
-
-# icon at index 10 is used when the battery is charging
-if args.icons:
-    icons = ["󰁺","󰁻","󰁼","󰁽","󰁾","󰁿","󰂀","󰂁","󰂂","󱟢","󰂄"]
-else:
-    icons = ["","","","","","","","","","",""]
-
-# icons[10] is not included in icons_colors on purpose because
-# icons_colors needs to be 10 long (see icon picking formula below)
-icons_colors = [
-    red+icons[0],
-    red+icons[1],
-    red+icons[2],
-    amber+icons[3],
-    amber+icons[4],
-    amber+icons[5],
-    green+icons[6],
-    green+icons[7],
-    green+icons[8],
-    green+icons[9]
-    ]
 
 def cleanup_output(raw_output):
     clean_output = raw_output.replace("b","").replace("\\n","").replace("'","")
@@ -77,6 +33,51 @@ def create_output(batteries_info):
     return " ".join(output)
 
 def main():
+    parser = argparse.ArgumentParser(
+    prog="Battery levels simple script",
+    description="reads the hwmon sys files to detect batteries input and outputs battery levels"
+    )
+
+    parser.add_argument("--icons",action='store_true')
+    parser.add_argument("--no-icons",dest='icons',action='store_false')
+    parser.add_argument("--colors",action='store_true')
+    parser.add_argument("--no-colors",dest='colors',action='store_false')
+    parser.set_defaults(icons=True,colors=True)
+    args = parser.parse_args()
+
+
+    if args.colors:
+        green="^c#409960^"
+        amber="^c#998240^"
+        red="^c#ba1818^"
+        white="^c#f2faf4^"
+    else:
+        green=""
+        amber=""
+        red=""
+        white=""
+
+    # icon at index 10 is used when the battery is charging
+    if args.icons:
+        icons = ["󰁺","󰁻","󰁼","󰁽","󰁾","󰁿","󰂀","󰂁","󰂂","󱟢","󰂄"]
+    else:
+        icons = ["","","","","","","","","","",""]
+
+    # icons[10] is not included in icons_colors on purpose because
+    # icons_colors needs to be 10 long (see icon picking formula below)
+    icons_colors = [
+        red+icons[0],
+        red+icons[1],
+        red+icons[2],
+        amber+icons[3],
+        amber+icons[4],
+        amber+icons[5],
+        green+icons[6],
+        green+icons[7],
+        green+icons[8],
+        green+icons[9]
+        ]
+
     batteries_info = get_batteries_info()
     output = create_output(batteries_info)
 

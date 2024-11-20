76d97bfe65a1ca1db45ac4ec62824d7e3f042b5e
gsebos <sgsebastiengeorges@gmail.com>
2024-11-20 18:46:15 +0000

    adds try except statement to catch any incorrect url

--- archnews.py
+++ archnews.py
@@ -3,11 +3,18 @@ import requests
 
 URL = "https://archlinux.org/"
 
-response = requests.get(URL)
-html = response.content
-soup = BeautifulSoup(html,features="html.parser")
-
-news = soup.find('div',class_='article-content').find('p').text
-date = soup.find('h4').find('a').find_next('p').text
-
-print(f"Latest Arch Wiki News: {date} - {news}")
+try:
+    response = requests.get(URL)
+except:
+    print(f"error with the URL, no response")
+    exit()
+if response.status_code == 200:
+    html = response.content
+    soup = BeautifulSoup(html,features="html.parser")
+    
+    news = soup.find('div',class_='article-content').find('p').text
+    date = soup.find('h4').find('a').find_next('p').text
+    
+    print(f"Latest Arch Wiki News: {date} - {news}")
+else:
+    print(f"Error, response status code {response.status_code}")

b9d8b61bfdc8b0e7f0a9935061853897bb17e070
gsebos <sgsebastiengeorges@gmail.com>
2025-10-14 14:16:19 +0100

    adds option for prefix and ability to backup to non-empty folder

--- backup.py
+++ backup.py
@@ -4,18 +4,20 @@ from datetime import date
 import shutil
 import argparse
 
-def get_newest_or_oldest_folder(BACKUP_PARENT, mode):
+def get_newest_or_oldest_folder(BACKUP_PARENT, mode, PREFIX):
     if mode not in ("newest","oldest"):
          raise ValueError("mode must be either 'newest' or 'oldest'")
     
     folders = os.listdir(path = BACKUP_PARENT)
     folders.sort(reverse = (mode == "newest"))
 
-    return folders[0], folders
+    backup_folders = [ folder for folder in folders if os.path.isdir( BACKUP_PARENT + folder) and folder.startswith(PREFIX) ]
 
-def do_backup(BACKUP_PARENT,SOURCE, MAX_NUM_BACKUPS,todays_date,dryrun):
+    return backup_folders[0], backup_folders
+
+def do_backup(BACKUP_PARENT,SOURCE, MAX_NUM_BACKUPS,todays_date,dryrun, PREFIX):
     # make sure the backup folder exists, if not create it
-    save_folder = f"backup_{todays_date}"
+    save_folder = f"{PREFIX}_{todays_date}"
     if not save_folder in os.listdir(BACKUP_PARENT):
         os.mkdir(BACKUP_PARENT + save_folder) 
 
@@ -42,8 +44,8 @@ def do_backup(BACKUP_PARENT,SOURCE, MAX_NUM_BACKUPS,todays_date,dryrun):
 
     print(f"######### back up {todays_date} #########" )
 
-    # get the oldest folder and the list of backup folders already present
-    oldest_folder, folders = get_newest_or_oldest_folder(BACKUP_PARENT,"oldest")
+    # get the oldest backup folder and the list of backup folders already present
+    oldest_folder, folders = get_newest_or_oldest_folder(BACKUP_PARENT,"oldest", PREFIX)
 
     if len(folders) > MAX_NUM_BACKUPS:
         # first delete the oldest backup
@@ -74,6 +76,7 @@ def main():
     
     parser.add_argument("--maxbackups", type = int , default = 5, help="when limit is reached, oldest foler is overwritten")
     parser.add_argument("--source", type = str , default = "/home/seb/",help="default is /home/seb/")
+    parser.add_argument("--prefix", type = str , default = "ez-rsync-backup",help="default is backup")
     parser.add_argument("--dest", type = str , default = "/mnt/casita-share/pc_backups/", help="default is /mnt/casita-share/pc_backups/")
     parser.add_argument("--live",action='store_true', help="if not present rsync runs in dry run mode")
     args = parser.parse_args()
@@ -81,6 +84,7 @@ def main():
     MAX_NUM_BACKUPS = args.maxbackups
     BACKUP_PARENT = args.dest
     SOURCE = args.source
+    PREFIX = args.prefix
     dryrun = not args.live
 
     todays_date = date.today().strftime("%Y-%m-%d")
@@ -98,8 +102,8 @@ def main():
             print(error)
         exit()
     
-    do_backup(BACKUP_PARENT,SOURCE,MAX_NUM_BACKUPS,todays_date,dryrun)
-
+    do_backup(BACKUP_PARENT,SOURCE,MAX_NUM_BACKUPS,todays_date,dryrun, PREFIX)
+   
     
 if __name__ == "__main__":
     main()

93e4ce24cccaca9a8c523e15378ef63f205e924d
gsebos <sgsebastiengeorges@gmail.com>
2025-10-15 10:42:15 +0100

    improves print messages

--- backup.py
+++ backup.py
@@ -60,16 +60,18 @@ def main():
     user = getpass.getuser()
     parser = argparse.ArgumentParser(
     prog="python backup.py",
-    description="a simple programme to create backups using rsync"
+    description="""A simple programme to create backups using rsync and automates the backup rotation. 
+    This creates a new folder everytime the programme runs. 
+    The maximum number of folder is defined by the --maxbackups flag, default is 5 """
     )
 
     parser.add_argument("--maxbackups", type = int , default = 5, help="when limit is reached, oldest foler is overwritten")
     parser.add_argument("--source", type = str , default = f"/home/{user}/",help=f"default is /home/{user}/")
-    parser.add_argument("--flags", type = str , default = "--delete --delete-excluded --include .cache/ --include .cache/mozilla/*** --exclude Games/*** --exclude .cache/*** --exclude .steam/*** --exclude Zomboid/*** --exclude .local/share/Steam/***",help="flags space separated")
-    parser.add_argument("--prefix", type = str , default = "pyrsync-backup",help="default is pyrsync-backup")
     parser.add_argument("--dest", type = str , default = "/mnt/casita-share/pc_backups/", help="default is /mnt/casita-share/pc_backups/")
+    parser.add_argument("--flags", type = str , default = "--delete --delete-excluded --exclude Games/*** --exclude .cache/*** --exclude .steam/*** --exclude Zomboid/*** --exclude .local/share/Steam/***",help="flags space separated")
+    parser.add_argument("--prefix", type = str , default = "pyrsync-backup",help="default is pyrsync-backup")
     parser.add_argument("--live",action='store_true', help="if not present rsync runs in dry run mode")
-    parser.add_argument("--silent",action='store_true', help="no -v flag")
+    parser.add_argument("--silent",action='store_true', help="no -v flag, runs without output")
     args = parser.parse_args()
 
     MAX_NUM_BACKUPS = args.maxbackups
@@ -103,19 +105,27 @@ def main():
     print()
     print(f"[INFO] rsync command: {command[1]}")
     print()
+    print("for help on how to change the rsync command, run python backup.py --help")
+    print()
 
-    confirm = input(f"You are about to back up the content of {SOURCE} into {save_path} (type yes to confirm)")
+    if dryrun:
+        print("[INFO] Running in dry-run mode...")
+        print(f"[INFO] Source would be {SOURCE}")
+        print(f"[INFO] destination would be {save_path}")  
+        print()
+        run_dryrun = input("Press any key to continue dryrun mode, q to exit. To run the back up, rerun the command with the --live flag\n")
+        if run_dryrun == "q":
+            print("programme exited")
+            exit()
+    else:
+        confirm = input(f"You are about to back up the content of {SOURCE} into {save_path} (type yes to confirm)")
 
-    if not confirm == "yes".lower():
-        print("backup aborted")
-        exit()
+        if not confirm == "yes".lower():
+            print("backup aborted")
+            exit()
 
     do_backup(BACKUP_PARENT,SOURCE,MAX_NUM_BACKUPS,todays_date, command[0],PREFIX)
-        
-    if dryrun:
-        print("[WARNING] This was a dry run, no back up was completed! Add the --live flag to backup")
-        print(f"[INFO] Source would be {SOURCE}")
-        print(f"[INFO] destination would be {save_path}")       
+                
     
 if __name__ == "__main__":
     main()
